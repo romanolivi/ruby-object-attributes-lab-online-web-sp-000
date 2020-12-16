9df8592708d736eeb1e25586f1ea3b633740259f
Romano Livi <romano_livi@outlook.com>
2020-12-16 18:38:06 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,17 @@
 class Dog 
   
   def initialize(name, breed)
+    @name = name
+    @breed = breed
   end
   
-  def name=()
+  def name
+    @name
+  end
+  
+  def name=(new_name)
+    @name = new_name
+  end
   
   
 end
